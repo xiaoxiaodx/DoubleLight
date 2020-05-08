5c1222791b5b22bf8e558e4e2cfa1a72285e4110
xiaoxiaodx <363904613@qq.com>
2020-05-08 17:44:10 +0800

    s

--- productiontool.cpp
+++ productiontool.cpp
@@ -7,14 +7,25 @@ ProductionTool::ProductionTool(QObject *parent) : QObject(parent)
     connect(&m_Process,SIGNAL(readyReadStandardOutput()),this,SLOT(slot_consoleOutput()));
 }
 
-void ProductionTool::funStartCmd(QString str)
+void ProductionTool::funStartCmd(QString deviceid)
 {
-    m_Process.start(str);
+
+    QString cmd = "act_dv300_common.exe "+deviceid+" "+deviceid+" 5d7073359bb502922d0b0566b8d8a6eb" + " 883a63d3af6569f8997b625bd1162966f92139af";
+
+
+
+    QFile file("lisence.key");
+    if(file.open(QIODevice::WriteOnly | QIODevice::Append)){
+        QTextStream  in(&file);
+        in<<cmd<<endl;
+        file.close();
+    }
+    m_Process.start(cmd);
 }
 
 void ProductionTool::slot_consoleOutput()
 {
-    qDebug()<<"dsadsad";
+
     QByteArray qbt = m_Process.readAllStandardOutput();
     QString msg = QString::fromLocal8Bit(qbt);
 
