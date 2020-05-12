76c27c7c411d189f5e72c9ab4389327905938903
xiaoxiaodx <363904613@qq.com>
2020-05-12 18:13:55 +0800

    s

--- protocol/tcpworker.cpp
+++ protocol/tcpworker.cpp
@@ -330,7 +330,7 @@ void TcpWorker::parseRecevieData()
                 else if(myType == 1){
                     parseShiGanRgb1(readDataBuff,m_streamDateLen,vResW,vResH);
                 }else if(myType == 2){
-                   parseShiGanRgb1(readDataBuff,m_streamDateLen,vResW,vResH);
+                   parseShiGanRgb2(readDataBuff,m_streamDateLen,vResW,vResH);
                 }
 
                 readDataBuff.remove(0,m_streamDateLen);
@@ -561,8 +561,30 @@ void TcpWorker::parseShiGanRgb1(QByteArray arr,int arrlen,int resw,int resh)
     }
     //qDebug()<<" parseShiGanRgb1 "<<resw<<"  "<<resh<<"  "<<arrlen <<"   "<< pImg->width() <<"  "<<pImg->height();
     emit signal_sendImg(pImg,arrlen,10,resw,resh);
-
 }
+
+void TcpWorker::parseShiGanRgb2(QByteArray arr,int arrlen,int resw,int resh)
+{
+
+
+    qDebug()<<" resw    "<<resw<<"  resh:"<<resh;
+    if(pNetMsgTmp == nullptr)
+        pNetMsgTmp = new unsigned char[resw * resh* 4];
+
+    memcpy(pNetMsgTmp,arr.data(),arrlen);
+
+    QImage *pImg = nullptr;
+    try {
+        pImg =  new QImage(pNetMsgTmp, resw,resh, QImage::Format_RGB32);
+        // 其它代码
+    } catch ( const std::bad_alloc& e ) {
+        qDebug()<<" 图片分配内存失败";
+        pImg = nullptr;
+    }
+    //qDebug()<<" parseShiGanRgb1 "<<resw<<"  "<<resh<<"  "<<arrlen <<"   "<< pImg->width() <<"  "<<pImg->height();
+    emit signal_sendImg(pImg,arrlen,10,resw,resh);
+}
+
 void TcpWorker::parseH264(QByteArray arr,int arrlen)
 {
 
